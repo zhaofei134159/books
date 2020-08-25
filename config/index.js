da324d4354e83704fb4617e64689268f8bd8e327
zhaofei <zhaofei@51smart.com>
2020-08-25 15:36:50 +0800

    修改

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '127.0.0.1', // can be overwritten by process.env.HOST
+    host: '104.243.18.161', // can be overwritten by process.env.HOST
     port: 8089, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     disableHostCheck: true,
     autoOpenBrowser: false,
