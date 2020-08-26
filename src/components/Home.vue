9d90f98ac7bbe1b72d30eea663d631f20c07c2a6
zhaofei <zhaofei@51smart.com>
2020-08-26 18:18:23 +0800

    修改

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,32 @@
 <template>
 	<div id="bodyDiv">
 		<el-container id="containerDiv">
-			<el-header>Header</el-header>
+			<el-header>
+				<el-menu
+				:default-active="activeIndex2"
+				class="el-menu-demo"
+				mode="horizontal"
+				@select="handleSelect"
+				background-color="#545c64"
+				text-color="#fff"
+				active-text-color="#ffd04b">
+					<el-menu-item index="1">处理中心</el-menu-item>
+					<el-submenu index="2">
+						<template slot="title">我的工作台</template>
+						<el-menu-item index="2-1">选项1</el-menu-item>
+						<el-menu-item index="2-2">选项2</el-menu-item>
+						<el-menu-item index="2-3">选项3</el-menu-item>
+						<el-submenu index="2-4">
+							<template slot="title">选项4</template>
+							<el-menu-item index="2-4-1">选项1</el-menu-item>
+							<el-menu-item index="2-4-2">选项2</el-menu-item>
+							<el-menu-item index="2-4-3">选项3</el-menu-item>
+						</el-submenu>
+					</el-submenu>
+					<el-menu-item index="3" disabled>消息中心</el-menu-item>
+					<el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+				</el-menu>
+			</el-header>
 			<el-container>
 				<el-aside width="200px">Aside</el-aside>
 				<el-main>Main</el-main>
@@ -16,7 +41,13 @@ export default {
   name: 'Home',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      activeIndex: '1',
+      activeIndex2: '1'
+    }
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
     }
   }
 }
@@ -25,7 +56,7 @@ export default {
 <style>
 	body{margin:0px;padding:0px;}
 	#bodyDiv{width:100%;}
-	#containerDiv{width:80%;margin:0 auto}
+	#containerDiv{width:65%;margin:0 auto}
 	.el-header, .el-footer {
 		background-color: #B3C0D1;
 		color: #333;
