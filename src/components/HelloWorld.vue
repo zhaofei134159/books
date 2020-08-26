bd1ff4df9dfe13cc26052dbdb7bccadce0cb54aa
zhaofei <zhaofei@51smart.com>
2020-08-26 11:05:16 +0800

    修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
+  <div class="bodyDiv">
     <el-row>
       <el-col :span="4"><div class="grid-content"></div></el-col>
       <el-col :span="16">
-        <div class="grid-content">{{msg}}</div>
+        <div class="grid-content top">{{msg}}</div>
+        <div class="grid-content content">{{msg}}</div>
+        <div class="grid-content footer">{{msg}}</div>
       </el-col>
       <el-col :span="4"><div class="grid-content"></div></el-col>
     </el-row>
@@ -22,6 +24,7 @@ export default {
 </script>
 
 <style>
+  .bodyDiv{margin:0px;padding:0px;}
   .el-row {
     margin-bottom: 20px;
     &:last-child {
@@ -35,8 +38,7 @@ export default {
     border-radius: 4px;
     min-height: 36px;
   }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
-  }
+  .top{width:100%;height:200px;background:red;}
+  .content{width:100%;min-height:800px;background:blue;}
+  .footer{width:100%;height:200px;background:yellow;}
 </style>
