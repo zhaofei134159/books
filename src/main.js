1c0bf3077ac14043d6d333cec380f2b5b0d163eb
zhaofei <zhaofei@51smart.com>
2020-08-26 16:28:08 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -2,9 +2,20 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
+
+// css 
+import 'static/bootstrap/css/bootstrap.css'
+import 'static/fonts/font-awesome/css/font-awesome.css'
+import 'static/fonts/fontello/css/fontello.css'
+import 'static/plugins/magnific-popup/magnific-popup.css'
+import 'static/css/animations.css'
+import 'static/plugins/owl-carousel/owl.carousel.css'
+import 'static/css/style.css'
+import 'static/css/css/skins/red.css'
+import 'static/css/custom.css'
+
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
