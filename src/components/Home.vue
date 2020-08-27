c17da63f63f9cc1a653cc2f579c7ba93d29623a7
zhaofei <13700530350@163.com>
2020-08-28 00:46:20 +0800

    1231

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -40,7 +40,7 @@
 				</div>
 				<div class="mainCont">
 					<el-row>
-						<el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 1 : 0">
+						<el-col :span="5" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 1 : 0">
 							<el-card :body-style="{ padding: '0px' }">
 								<img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
 								<div class="title">
