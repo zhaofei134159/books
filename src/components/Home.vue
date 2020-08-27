9923a43af35c77448ca08658300217854fa2bb91
zhaofei <13700530350@163.com>
2020-08-28 00:53:51 +0800

    修改

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -41,7 +41,7 @@
 				<div class="mainCont">
 					<el-row>
 						<el-col :span="5" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 1 : 0">
-							<el-card :body-style="{ padding: '0px' }">
+							<el-card>
 								<img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
 								<div class="title">
 									<span>好吃的汉堡</span>
