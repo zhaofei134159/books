7753b46b2db0b310d8048f4801f574460bb2b435
zhaofei <zhaofei@51smart.com>
2020-08-27 09:43:31 +0800

    修改

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -38,9 +38,9 @@
 		</div>
 		<div id="footerDiv">
 			<div class="widewrapper">
-            	© 2015 family 来自 <a href="http://blog.myfeiyou.com/" target="_blank" title="family">family</a> 
-            	<br>
-            	<a href="http://www.beian.miit.gov.cn" target="__black">京ICP备16026346号-1</a>
+				© 2015 family 来自 <a href="http://blog.myfeiyou.com/" target="_blank" title="family">family</a> 
+				<br>
+				<a href="http://www.beian.miit.gov.cn" target="__black">京ICP备16026346号-1</a>
 			</div>
 		</div>
 	</div>
