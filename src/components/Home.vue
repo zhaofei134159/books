47baac21cf898b4a17d91073f53ce18ea6aad64c
zhaofei <13700530350@163.com>
2020-08-28 00:31:14 +0800

    1231

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -43,7 +43,7 @@
 						<el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
 							<el-card :body-style="{ padding: '0px' }">
 								<img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
-								<div style="padding: 14px;">
+								<div>
 									<span>好吃的汉堡</span>
 									<div class="bottom clearfix">
 										<time class="time">{{ currentDate }}</time>
