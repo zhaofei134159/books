5977c4f166fda6426e6544a27006a597bfa90000
zhaofei <zhaofei@51smart.com>
2020-08-28 11:30:05 +0800

    修改

--- src/components/public/publicFooter.vue
+++ src/components/public/publicFooter.vue
@@ -1,8 +1,8 @@
 <template>
 	<div id="footerDiv">
-		<div>
+		<template>
 			<el-backtop target=".page-component__scroll .el-scrollbar__wrap"></el-backtop>
-		</div>
+		</template>
 		<div class="widewrapper">
 			© 2015 family 来自 <a href="http://blog.myfeiyou.com/" target="_blank" title="family">family</a>
 			<a href="http://www.beian.miit.gov.cn" target="__black">京ICP备16026346号-1</a>
