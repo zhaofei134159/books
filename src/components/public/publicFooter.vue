69c3f04fd0914bdd056511cad3c0b4cd5ae54df7
zhaofei <zhaofei@51smart.com>
2020-08-28 11:35:05 +0800

    修改

--- src/components/public/publicFooter.vue
+++ src/components/public/publicFooter.vue
@@ -1,12 +1,26 @@
 <template>
 	<div id="footerDiv">
-		<template>
-			<el-backtop target=".page-component__scroll .el-scrollbar__wrap"></el-backtop>
-		</template>
 		<div class="widewrapper">
 			© 2015 family 来自 <a href="http://blog.myfeiyou.com/" target="_blank" title="family">family</a>
 			<a href="http://www.beian.miit.gov.cn" target="__black">京ICP备16026346号-1</a>
 		</div>
+		<template>
+			<el-backtop target=".page-component__scroll .el-scrollbar__wrap" :bottom="100">
+				<div
+					style="{
+					height: 100%;
+					width: 100%;
+					background-color: #f2f5f6;
+					box-shadow: 0 0 6px rgba(0,0,0, .12);
+					text-align: center;
+					line-height: 40px;
+					color: #1989fa;
+					}"
+				>
+					<i class="el-icon-arrow-up"></i>
+				</div>
+			</el-backtop>
+		</template>
 	</div>
 </template>
 
