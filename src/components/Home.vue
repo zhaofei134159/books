a34f6e64fe5c4e8998fe16a879d9ec8f4692c9ea
zhaofei <zhaofei@51smart.com>
2020-08-28 09:41:07 +0800

    修改

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,7 +20,7 @@
 							<el-card>
 								<img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
 								<div class="title">
-									<span>好吃的汉堡</span>
+									<span>好吃的汉堡 {{ index }}</span>
 									<div class="bottom clearfix">
 										<time class="time">{{ currentDate }}</time>
 										<el-button type="text" class="button">操作按钮</el-button>
