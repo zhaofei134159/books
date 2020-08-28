475931135dfea1a3cc34e2a92a73350f68e39833
zhaofei <zhaofei@51smart.com>
2020-08-28 15:48:56 +0800

    修改

--- src/request/http.js
+++ src/request/http.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 import { Message, Loading } from 'element-ui'
-const ConfigBaseURL = 'https://localhost:3000/' //默认路径，这里也可以使用env来判断环境
-let loadingInstance = null //这里是loading
+const ConfigBaseURL = 'https://localhost:3000/' // 默认路径，这里也可以使用env来判断环境
+let loadingInstance = null // 这里是loading
 
-//使用create方法创建axios实例
+// 使用create方法创建axios实例
 export const Service = axios.create({
   timeout: 7000, // 请求超时时间
   baseURL: ConfigBaseURL,
