02ac7863d4d324cb3a4bbf3bb62c8679d68f6cac
zhaofei <zhaofei@51smart.com>
2020-08-28 18:03:13 +0800

    修改

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -64,7 +64,7 @@ export default {
     var self = this
     getHomeByBooks().then(res => {
       if (res.errorNo === '0') {
-        for(let index,item of res.seccuss) {
+        for (let index,item in res.seccuss) {
           res.seccuss[index].src = self.sourceUrl+'/'+item.src
         }
         self.cardData = res.seccuss
