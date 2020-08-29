b9925e92f0633c355d327e3018a5b2ce355b0558
zhaofei <13700530350@163.com>
2020-08-30 01:36:41 +0800

    修改

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,9 +15,9 @@
 					<el-row>
 						<el-col v-for="(val, index) in cardData" :span="5" :key="index" class="colClass">
 							<el-card class="cardDataClass">
-								<router-link tag="div" :to="'/chapter/chapterList' + val.id"> <img :src="val.src" class="image"> </router-link>
+								<router-link tag="div" :to="'/chapter/chapterList/' + val.id"> <img :src="val.src" class="image"> </router-link>
 								<div class="title">
-									<router-link  tag="div" :to="'chapter/chapterList' + val.id" :card="index">
+									<router-link  tag="div" :to="'/chapter/chapterList/' + val.id" :card="index">
 										{{ val.name }}
 										<time class="showDesc">{{ val.author }}</time>
 									</router-link>
