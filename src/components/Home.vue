28a998d61ae7024e242443c470924f818d5aab54
zhaofei <13700530350@163.com>
2020-08-29 23:20:28 +0800

    修改

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -79,6 +79,7 @@ export default {
           res.seccuss.works[index].src = self.sourceUrl + '/' + item.src
         })
         self.cardData = res.seccuss.works
+        console.log(self.cardData);
       } else {
         this.$message.error('请求错误, 请重试！')
       }
