087dd60a25c43e33b6e8f43ff3334ca81fd7204c
zhaofei <13700530350@163.com>
2020-08-30 01:54:33 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ export default new Router({
     },
     {
       path: '/chapter/chapterList/:workId',
-      name: 'chapterLis',
+      name: '/chapter/chapterList/',
       component: chapterLis
     }
   ]
