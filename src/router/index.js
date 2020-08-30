8a21398a002b2406404f668a07d2606e2e655c2e
zhaofei <13700530350@163.com>
2020-08-30 18:47:22 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import ChapterList from '@/components/chapter/chapterList'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   routes: [
     {
       path: '/',
