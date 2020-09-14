353bef2ff7a7d03c2d7ebda006e5638f635b3e2d
zhaofei <zhaofei@51smart.com>
2020-09-14 15:08:27 +0800

    修改

--- src/components/public/publicNav.vue
+++ src/components/public/publicNav.vue
@@ -5,7 +5,7 @@
 			<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
 				<el-menu-item index="1">首页</el-menu-item>
 				<el-menu-item index="2">聊天室</el-menu-item>
-				<el-menu-item index="3"><a href="https://blog.myfeiyou.com/home/about" target="_blank">关于我们</a></el-menu-item>
+				<el-menu-item index="3">关于我们</el-menu-item>
 				<!-- <el-submenu index="2">
 				<template slot="title">我的工作台</template>
 					<el-menu-item index="2-1">选项1</el-menu-item>
@@ -41,6 +41,8 @@ export default {
         this.$router.push({name: 'Home'})
       } else if (key === '2') {
         this.$router.push({name: 'ChatRoom'})
+      } else if (key === '3') {
+        window.location.href = 'https://blog.myfeiyou.com/home/about';
       }
     }
   }
