57f75c4fa815b99cb732fd7969488438e0aacea3
zhaofei <zhaofei@51smart.com>
2020-09-21 14:28:40 +0800

    book 跳转登陆

--- src/components/public/publicTop.vue
+++ src/components/public/publicTop.vue
@@ -5,7 +5,7 @@
 				<div @click="linkHome()">booksfamily</div>
 			</div>
 			<div class="headerRight">
-        <div @click="linkLogin()">登录</div>
+        <!-- <div @click="linkLogin()">登录</div> -->
       </div>
 		</div>
 	</div>
