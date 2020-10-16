4cb9b3e65f9394de50f5c05e37cac5cc22031966
zhaofei <zhaofei@51smart.com>
2020-10-16 18:00:39 +0800

    图展

--- src/components/picture/pictureList.vue
+++ src/components/picture/pictureList.vue
@@ -82,8 +82,7 @@ export default {
     handleScroll() {
       let a = this.el.getBoundingClientRect().bottom
       a = Math.ceil(a)
-      
-      if (a * page >= (this.clientHeight - 100)) {
+      if (a * this.page >= (this.clientHeight - 100)) {
         this.isbottom = -1
         this.page++
         this.getPictureListPage(this.page)
