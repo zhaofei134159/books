e895027d577f58dbe57c68a1c9e800e3bf1d689f
zhaofei <zhaofei@51smart.com>
2020-10-16 10:20:10 +0800

    图展

--- src/router/index.js
+++ src/router/index.js
@@ -41,8 +41,9 @@ export default new Router({
       }
     },
     {
-      path: '/picture/pictureExhibit',
+      path: '/picture/pictureExhibit/',
       name: 'PictureList',
+      props: true, // 若个要解耦的 到组件中 props 中。
       component: PictureList,
       meta: {
         title: '图展'
